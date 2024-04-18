3666beea2958246e5c323245d9ea1cdb246dc481
mtwilkinson <122630959+mtwilkinson@users.noreply.github.com>
2024-04-18 13:01:56 -0400

    added divs that change color when clicked

--- cs4241-websockets/src/App.svelte
+++ cs4241-websockets/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   let msgs = []
+  let TL = ["grey", "grey", "grey", "grey", "grey", "grey", "grey", "grey", "grey"];
 
   const ws = new WebSocket( 'ws://127.0.0.1:3000' )
 
@@ -20,9 +21,50 @@
     ws.send( txt )
     msgs = msgs.concat([ 'me: ' + txt ])
   }
-</script>
 
-<input type='text' on:change={send} />
+  function flipTL(i) {
+      if (TL[i] === "grey") {
+          TL[i] = "blue"
+      } else {
+          TL[i] = "grey"
+      }
+  }
+
+
+</script>
+<style>
+    .row {
+        display: flex;
+    }
+
+    div {
+        min-height: 100px;
+        min-width: 100px;
+    }
+</style>
+
+<div>
+    <input type='text' on:change={send} />
+</div>
+
+<div>
+    <div class="row">
+        <div on:click={() => flipTL(0)} style="background-color: {TL[0]}; margin: 6px"></div>
+        <div on:click={() => flipTL(1)} style="background-color: {TL[1]}; margin: 6px"></div>
+        <div on:click={() => flipTL(2)} style="background-color: {TL[2]}; margin: 6px"></div>
+    </div>
+    <div class="row">
+        <div on:click={() => flipTL(3)} style="background-color: {TL[3]}; margin: 6px"></div>
+        <div on:click={() => flipTL(4)} style="background-color: {TL[4]}; margin: 6px"></div>
+        <div on:click={() => flipTL(5)} style="background-color: {TL[5]}; margin: 6px"></div>
+    </div>
+    <div class="row">
+        <div on:click={() => flipTL(6)} style="background-color: {TL[6]}; margin: 6px"></div>
+        <div on:click={() => flipTL(7)} style="background-color: {TL[7]}; margin: 6px"></div>
+        <div on:click={() => flipTL(8)} style="background-color: {TL[8]}; margin: 6px"></div>
+    </div>
+</div>
+
 
 {#each msgs as msg }
   <h3>{msg}</h3>
