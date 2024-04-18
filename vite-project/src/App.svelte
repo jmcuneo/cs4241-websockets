c5e41d656d71e9605abc5751c6259347e9ce29c5
ibixler <108148378+ibixler@users.noreply.github.com>
2024-04-18 12:40:09 -0400

    farts

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,47 +1,41 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<html lang="en">
+  <head>
+    <style>
+      body { 
+        margin:0; 
+        background:black 
+      } 
+    </style>
+    <script>
+      let ws, msgs = [], ctx = null
+      
+      window.onload = function() {
+        ws = new WebSocket( 'ws://127.0.0.1:3000' )
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+        ws.onopen = () => {
+          ws.onmessage = async msg => {
+            const pos = await msg.data.text()
+            const [x,y] = pos.split( ':' ).map( v => parseInt(v) )
 
-  <div class="card">
-    <Counter />
-  </div>
+            ctx.fillStyle = 'red'
+            ctx.fillRect( x,y,50,50 )
+          }
+        }
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+        const canvas = document.querySelector('canvas')
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        ctx = canvas.getContext( '2d' )
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+        window.onclick = e => {
+          ws.send( `${e.pageX}:${e.pageY}` )
+          ctx.fillStyle = 'yellow'
+          ctx.fillRect( e.pageX,e.pageY,50,50 )
+        }
+      }
+    </script>
+  </head>
+  <body>
+    <canvas></canvas>
+  </body>
+</html>
